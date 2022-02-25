20cd3423ffdec697b5db2924c2bd478b9d09db64
riggage <riggage@hotmail.co.uk>
2022-02-25 12:29:34 +0000

    update and delete are on

---
+++ src/app.js
@@ -0,0 +1,27 @@
+require("./db/connection")
+const mongoose = require("mongoose")
+const yargs = require("yargs")
+const { addMovie, list, update, deleteOne } = require("./movie/functions")
+
+const app = async (yargsObj) => {
+    try {
+        if (yargsObj.add) {
+            console.log(await addMovie(yargsObj.title, yargsObj.actor))
+            // add functionality
+        } else if (yargsObj.list) {
+            console.log(await list())
+            // list functionality
+        } else if (yargsObj.update) {
+            console.log(await update(yargsObj.title, yargsObj.actor))
+        } else if (yargsObj.delete){
+            console.log(await deleteOne(yargsObj.title))
+        } else {
+            console.log("Incorrect command")
+        }
+        await mongoose.disconnect()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+app(yargs.argv)
---
+++ src/movie/functions.js
@@ -0,0 +1,43 @@
+const Movie = require("./model");
+
+exports.addMovie = async (title, actor) => {
+  try {
+    await Movie.create({ title: title, actor: actor });
+    return "Success";
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.list = async () => {
+  try {
+    return await Movie.find({});
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.update = async (title, actor) => {
+  try {
+      await Movie.updateOne(
+        {title: title},
+        {$set:{
+              actor: actor}
+        }
+      )
+      return "Updated"
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+exports.deleteOne = async (title) =>{
+  try {
+      await Movie.deleteOne(
+        {title: title}
+      )
+      return "Entry removed"
+  } catch (error) {
+    console.log(error)
+  }
+}
